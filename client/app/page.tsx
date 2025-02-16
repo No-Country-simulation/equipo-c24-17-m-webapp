8b017c561befa491275c02ff24cd08c2a34ffc7f
Matias Luduena <matiasluduena23@gmail.com>
2025-02-16 20:27:50 -0300

    setup client nextjs

---
+++ client/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+	return (
+		<main className="flex flex-col gap-8 row-start-2 items-center mt-20">
+			<h1 className="text-3xl">Proyecto No Country</h1>
+		</main>
+	);
+}
