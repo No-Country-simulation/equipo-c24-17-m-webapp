fe37914db5cbcaf3515898a5e1c7e95b4d5b6460
Matias Luduena <matiasluduena23@gmail.com>
2025-02-17 16:41:08 -0300

    deploy- agregar docker compose para ejecutar la BD

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+510eb7fc21b640d7e42a027ab303335b562930a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f3bfee9dc662d4eeaf6269a505442d002ef6835")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
