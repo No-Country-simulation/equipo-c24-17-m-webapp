510eb7fc21b640d7e42a027ab303335b562930a9
Matias Luduena <matiasluduena23@gmail.com>
2025-02-17 09:43:11 -0300

    cliente y server conectados para test deploy

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,15 @@
+import { getWeather } from "./action";
+
 export default async function Home() {
+	const data = await getWeather();
 	return (
 		<main className="flex flex-col gap-8 row-start-2 items-center mt-20">
 			<h1 className="text-3xl">Proyecto No Country</h1>
+			<ul>
+				{data.map((item, index) => (
+					<li key={index}>{item.summary}</li>
+				))}
+			</ul>
 		</main>
 	);
 }
---
+++ server/Program.cs
@@ -0,0 +1,65 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//configura CORS para permitir cualquier acceso
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+// Configura Kestrel para escuchar en ambos IPv4 and IPv6
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000); // Escucha en IPv4 (0.0.0.0) and IPv6 (::) en el puesto 5000
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+
+app.UseCors("AllowAll");
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
