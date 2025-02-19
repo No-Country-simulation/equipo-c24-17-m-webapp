6938c34e91f140d47c930caae3a4dfa2c17507ff
Matias Luduena <matiasluduena23@gmail.com>
2025-02-19 17:25:20 -0300

    ft-front-unit-test-autenticacion

---
+++ client/components/__test__/iniciar-sesion.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import IniciarSesion from "../iniciar-sesion";
+import { signIn } from "@/auth";
+
+vi.mock("@/auth", () => ({
+	signIn: vi.fn(),
+}));
+
+describe("IniciarSesion componente", () => {
+	it("renderizar el boton y la imagen de google", () => {
+		render(<IniciarSesion />);
+		const button = screen.getByRole("button", { name: /iniciar sesión/i });
+		expect(button).toBeInTheDocument();
+		const image = screen.getByAltText("google logo");
+		expect(image).toBeInTheDocument();
+		expect(image).toHaveAttribute("src", "/google-icon.svg");
+	});
+
+	it("ejecutar signin fn on click", async () => {
+		render(<IniciarSesion />);
+		const button = screen.getByRole("button", { name: /iniciar sesión/i });
+		await userEvent.click(button);
+		expect(signIn).toHaveBeenCalledWith("google", { redirectTo: "/panel" });
+	});
+});
