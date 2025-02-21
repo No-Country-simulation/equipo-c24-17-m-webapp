56fb3a337b29b1ee3324dcdf7113ba33d27c93f6
Matias Luduena <matiasluduena23@gmail.com>
2025-02-21 17:27:26 -0300

    redeploy

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default async function Home() {
 	return (
 		<main className="flex flex-col gap-8 row-start-2 items-center mt-20">
-			<h1 className="text-3xl">Proyecto No Country</h1>
+			<h1 className="text-3xl">Te acompaño</h1>
 			<Button>
 				<Link href={"/login"}>Iniciar Sesión</Link>
 			</Button>
