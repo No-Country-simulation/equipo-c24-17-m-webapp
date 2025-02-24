4b5286c5c6239e3ebb4d714a44ee9620aa7476ea
Gabriela <101139215+Gabriela1994@users.noreply.github.com>
2025-02-24 18:21:42 -0300

    Migraciones de la base de datos, se corrigieron problemas anteriores. (#10)
    * Migraciones realizadas con las tablas nuevas
    
    * fix controladores, fix mayusculas en endpoints

---
+++ server/Data/Models/Tipoestudio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace server.Data.Models;
+
+public partial class Tipoestudio
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string? Descripcion { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+    public virtual ICollection<Estudiosmedico> Estudiosmedicos { get; set; } = new List<Estudiosmedico>();
+}
--- server/Data/Models/Usuario.cs
+++ server/Data/Models/Usuario.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace server.Data.Models;
@@ -13,11 +12,10 @@ public partial class Usuario
 
     public string Correo { get; set; } = null!;
 
-    [ForeignKey("id_rol")]
-    public int IdRol { get; set; }
-
     public DateTime CreatedAt { get; set; }
 
+    public string? Imagen { get; set; }
+
     [JsonIgnore]
-    public virtual Rol? IdRolNavigation { get; set; } = null!;
+    public virtual ICollection<Hijo> Hijos { get; set; } = new List<Hijo>();
 }
