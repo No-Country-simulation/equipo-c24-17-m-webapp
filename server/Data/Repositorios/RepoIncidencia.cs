d4a7ee0b69a87691086fb6ab9e0c8c67fc289831
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-02-25 17:47:16 -0300

    Repositorio: repositorio de incidencias creado

---
+++ server/Data/Repositorios/RepoIncidencia.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Data.Models;
+
+namespace server.Data.Repositorios
+{
+    public class RepoIncidencia
+    {
+        #region ContextDataBase
+        private readonly BdTeacompanioContext _context;
+
+        public RepoIncidencia(BdTeacompanioContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+
+        public List<Incidencia> GetAll()
+        {
+            List<Incidencia> lista_incidencias = [];
+
+            try
+            {
+                return lista_incidencias = _context.Incidencias.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Incidencia GetById(int id)
+        {
+            Incidencia incidencia_obtenida = new();
+            try
+            {
+                return incidencia_obtenida = _context.Incidencias.Find(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void CreateIncidencia(Incidencia obj_incidencia)
+        {
+            try
+            {
+                _context.Add(obj_incidencia);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Inner Exception: {ex.InnerException.Message}");
+                }
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void UpdateIncidencia(Incidencia obj_incidencia)
+        {
+            try
+            {
+                _context.Incidencias.Update(obj_incidencia);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception("Error al actualizar la incidencia en la base de datos.", dbEx);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error general al actualizar al incidencia.", ex);
+            }
+        }
+
+        public void DeleteIncidencia(Incidencia obj_incidencia)
+        {
+            try
+            {
+                _context.Incidencias.Remove(obj_incidencia);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error general al eliminar la incidencia.", ex);
+            }
+        }
+    }
+}
