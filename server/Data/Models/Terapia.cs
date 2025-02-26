585d943665fe3e85527f66f76112414a647e0ec9
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-02-25 21:15:56 -0300

    Models: crear modelo terapia y tipoEspecialidad

---
+++ server/Data/Models/Terapia.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace server.Data.Models
+{
+    public class Terapia
+    {
+        public int Id { get; set; }
+        public DateOnly Fecha_inicio { get; set; }
+        public DateOnly Fecha_culminacion { get; set; }
+        public TimeOnly Horario { get; set; }
+        public bool Esta_activo { get; set; }
+        public int IdTipoEspecialidad { get; set; }
+
+        [JsonIgnore]
+        public virtual TipoEspecialidad? IdTipoEspecialidadNavigation { get; set; } = null!;
+    }
+}
