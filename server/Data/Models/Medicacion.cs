4c37264a7bc4ec42eaa8eb25f59632f7682627ab
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-02-26 13:25:28 -0300

    Modelo: creacion de modelo Medicacion

---
+++ server/Data/Models/Medicacion.cs
@@ -0,0 +1,16 @@
+﻿using server.Utils;
+using System.Text.Json.Serialization;
+
+namespace server.Data.Models
+{
+    public class Medicacion
+    {
+        public int Id { get; set; }
+        public int Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        [JsonConverter(typeof(TimeOnlyConverter))]
+        public TimeOnly Horario { get; set; }
+
+    }
+}
