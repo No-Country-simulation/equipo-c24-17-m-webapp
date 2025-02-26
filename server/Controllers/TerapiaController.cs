7808f09ced862c2686c1232eff77e2130cb0f7d8
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-02-26 11:41:42 -0300

    GET: obtener todas las terapias de un hijo

---
+++ server/Controllers/TerapiaController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using server.Data;
+using server.Data.Models;
+using server.Logica;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TerapiaController : ControllerBase
+    {
+        #region ContextDataBase
+        private readonly BdTeacompanioContext _context;
+
+        public TerapiaController(BdTeacompanioContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        // GET: api/<TerapiaController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Terapia>> GetAll(int id)
+        {
+            LogicaTerapia logica_terapia = new LogicaTerapia(_context);
+            try
+            {
+                var lista_terapias = logica_terapia.ObtenerTodosLasTerapias(id);
+                return Ok(lista_terapias);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(404, $"Error al obtener las terapias: {ex.Message}");
+            }
+        }
+
+        // GET api/<TerapiaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TerapiaController>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<TerapiaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<TerapiaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
