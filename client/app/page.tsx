2d555dd0ebcbc74eaa67e34b2209ae69cb484752
Matias Luduena <matiasluduena23@gmail.com>
2025-02-26 15:07:03 -0300

    fix-remove-comment

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,30 +1,8 @@
-
-// import {
-// NavigationMenu,
-// NavigationMenuContent,
-// NavigationMenuItem,
-// NavigationMenuLink,
-// NavigationMenuList,
-// NavigationMenuTrigger,
-// navigationMenuTriggerStyle,
-// } from "@/components/ui/navigation-menu"
-
-//import {
-	// Card,
-	// CardContent,
-	// CardDescription,
-	// CardFooter,
-	// CardHeader,
-	// CardTitle,
-//} from "@/components/ui/card"
-
 import Header from "@/components/Header";
 
-
 export default async function Home() {
-
 	return (
-		<div className="">
+		<div className=" max-w-[1100px]">
 			<Header />
 			<main className="flex flex-col gap-8 row-start-2 items-center mt-40">
 				<h1 className="text-3xl">Te acompaño</h1>
@@ -32,55 +10,3 @@ export default async function Home() {
 		</div>
 	);
 }
-
-
-// <NavigationMenu>-
-// 	<NavigationMenuList className=" grid-cols-6 gap-2">
-// 		<NavigationMenuItem>
-// 			<Link href="/#" legacyBehavior passHref>
-// 				<Button  variant={"TDA"} className="w">
-// 					<NavigationMenuLink className="">
-// 						Inicio
-// 					</NavigationMenuLink>
-// 				</Button>
-// 			</Link>
-// 		</NavigationMenuItem>
-// 		<NavigationMenuItem>
-// 			<Link href="/#" legacyBehavior passHref>
-// 				<Button variant={"TDA"} className="w">
-// 					<NavigationMenuLink className="">
-// 						Quienes Somos
-// 					</NavigationMenuLink>
-// 				</Button>
-// 			</Link>
-// 		</NavigationMenuItem>
-// 		<NavigationMenuItem className="translate-y-[-2px]">
-// 			<Link href="/#" legacyBehavior passHref>
-// 				<Button variant={"TDA"} className="w">
-// 					<NavigationMenuLink className="text-xs ">
-// 						Contenido <br />de Apoyo
-// 					</NavigationMenuLink>
-// 				</Button>
-// 			</Link>
-// 		</NavigationMenuItem>
-// 		<NavigationMenuItem>
-// 			<Link href="/#" legacyBehavior passHref>
-// 				<Button variant={"TDA"} className="">
-// 					<NavigationMenuLink className="">
-// 						Comunidad
-// 					</NavigationMenuLink>
-// 				</Button>
-// 			</Link>
-// 		</NavigationMenuItem>
-// 		<NavigationMenuItem>
-// 			<Link href="/#" legacyBehavior passHref>
-// 				<Button variant={"TDA"} className="">
-// 					<NavigationMenuLink className="">
-// 						FAQs
-// 					</NavigationMenuLink>
-// 				</Button>
-// 			</Link>
-// 		</NavigationMenuItem>
-// 	</NavigationMenuList>
-
-// </NavigationMenu>
