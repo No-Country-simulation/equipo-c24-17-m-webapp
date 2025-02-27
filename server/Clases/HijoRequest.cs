c373064687aed6db2c4fb3d5ab98795bbc5f9f7e
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-02-27 11:15:03 -0300

    Modificar funcionalidad: Logica de creacion de hijos

---
+++ server/Clases/HijoRequest.cs
@@ -0,0 +1,10 @@
+﻿using server.Data.Models;
+
+namespace server.Clases
+{
+    public class HijoRequest
+    {
+        public string CorreoUsuario { get; set; }
+        public Hijo Hijo { get; set; }
+    }
+}
--- server/Logica/LogicaHijo.cs
+++ server/Logica/LogicaHijo.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using server.Clases;
 using server.Data;
 using server.Data.Models;
 using server.Data.Repositorios;
@@ -48,15 +49,29 @@ namespace server.Logica
             }
         }
 
-        public void CrearHijo(Hijo obj_hijo)
+        public void CrearHijo(HijoRequest obj_request)
         {
             RepoHijo repo_hijo = new RepoHijo(_context);
             try
             {
-                var hijo_existe = _context.Hijos.Any(u => u.Id ==  obj_hijo.Id);
+                var hijo_existe = _context.Hijos.Any(u => u.Id == obj_request.Hijo.Id);
                 if (hijo_existe == true) throw new KeyNotFoundException("Ya hay un hijo cargado con ese id");
 
-                obj_hijo.CreatedAt = DateTime.SpecifyKind(obj_hijo.CreatedAt, DateTimeKind.Unspecified);
+
+                var obtener_id_usuario = _context.Usuarios.Where(x => x.Correo.Equals(obj_request.CorreoUsuario)).FirstOrDefault();
+                if(obtener_id_usuario == null) throw new KeyNotFoundException("No existe un usuario asociado a ese correo electrónico");
+
+                Hijo obj_hijo = new Hijo
+                {
+                    Nombre = obj_request.Hijo.Nombre,
+                    Apellido = obj_request.Hijo.Apellido,
+                    FechaNacimiento = obj_request.Hijo.FechaNacimiento,
+                    NombreDiagnostico = obj_request.Hijo.NombreDiagnostico,
+                    DescripcionDiagnostico = obj_request.Hijo.DescripcionDiagnostico,
+                    IdUsuario = obtener_id_usuario.Id,
+                    CreatedAt = DateTime.SpecifyKind(obj_request.Hijo.CreatedAt, DateTimeKind.Unspecified)
+                };
+
                 repo_hijo.CreateHijo(obj_hijo);
             }
             catch (Exception ex)
