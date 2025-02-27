e2d2f6fda29a8472e558f37cbc2e55e9fa38d0fd
Matias Luduena <matiasluduena23@gmail.com>
2025-02-27 19:59:51 -0300

    card pariente

--- client/app/panel/page.tsx
+++ client/app/panel/page.tsx
@@ -2,14 +2,21 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import CardPariente from "./card-pariente";
 import Link from "next/link";
+import { getParientes, getSession } from "@/lib/database";
 
 export default async function page() {
+	const sesion = await getSession();
+	const parientes = await getParientes(sesion.email);
+
 	return (
 		<div className="">
 			<main className="grid content-center min-h-screen justify-center gap-8">
-				<CardPariente />
+				{parientes.map((item) => (
+					<CardPariente key={item.id} pariente={item} />
+				))}
+
 				<Button variant={"outline"}>
-					<Link href={"/panel/pariente"}>Agregar pariente</Link>
+					<Link href={"/panel/pariente/crear"}>Agregar pariente</Link>
 				</Button>
 			</main>
 		</div>
--- client/lib/definitions.ts
+++ client/lib/definitions.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
-import { parienteSchema } from "./schemas";
+import { parienteSchema, parienteSchemaNoID } from "./schemas";
 
 export type ParienteT = z.infer<typeof parienteSchema>;
+
+export type KeyofPariente = keyof z.infer<typeof parienteSchemaNoID>;
