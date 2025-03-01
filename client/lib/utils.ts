68da96a0c9dcf81b87d55f0063f169d1875b1bb5
Matias Luduena <matiasluduena23@gmail.com>
2025-03-01 18:23:03 -0300

    ft-front-eliminar-dialog-eliminar-incidencia

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { FieldErrors, FormType } from "./definitions";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function handleFieldErrors<T>(err: FieldErrors, form: FormType<T>) {
+	Object.entries(err.fieldErrors).forEach(([field, error]) => {
+		form.setError(field as T, {
+			message: (error as string[])[0],
+		});
+	});
 }
