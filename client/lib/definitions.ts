628d8c9f9bc0b15b159d10ff514f6b34d9d2efbe
Matias Luduena <matiasluduena23@gmail.com>
2025-03-01 18:19:54 -0300

    schema & tipos incidencias

--- client/lib/definitions.ts
+++ client/lib/definitions.ts
@@ -1,6 +1,25 @@
 import { z } from "zod";
-import { parienteSchema, parienteSchemaNoID } from "./schemas";
+import { incidenciaSchema, parienteSchema } from "./schemas";
 
 export type ParienteT = z.infer<typeof parienteSchema>;
 
-export type KeyofPariente = keyof z.infer<typeof parienteSchemaNoID>;
+export type IncidenciaT = z.infer<typeof incidenciaSchema>;
+
+export interface FieldErrors {
+	fieldErrors: {
+		[key: string]: string[];
+	};
+}
+
+export interface FormType<T> {
+	setError: (name: T, error: { message: string }) => void;
+}
+
+export type TipoIncidenciaT = {
+	id: number;
+	nombre: string;
+};
+
+export type ParienteWithIncidenciasT = ParienteT & {
+	incidencias: IncidenciaT[];
+};
