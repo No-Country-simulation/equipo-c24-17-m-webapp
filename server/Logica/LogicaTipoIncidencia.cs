5d0dbbbd60b5deb261ab136ccc0587f1f3bb323a
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-02-28 22:25:27 -0300

    logica: logica de obtener el listado de tipos de incidencia

---
+++ server/Logica/LogicaTipoIncidencia.cs
@@ -0,0 +1,33 @@
+﻿using server.Data;
+using server.Data.Models;
+using server.Data.Repositorios;
+
+namespace server.Logica
+{
+    public class LogicaTipoIncidencia
+    {
+        #region ContextDataBase
+        private readonly BdTeacompanioContext _context;
+
+        public LogicaTipoIncidencia(BdTeacompanioContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        public List<TipoIncidencia> ObtenerTodasLosTiposDeIncidencias()
+        {
+            RepoTipoIncidencia repo_tipo_incidencia = new RepoTipoIncidencia(_context);
+            List<TipoIncidencia> lista_tipo_incidencias = [];
+
+            try
+            {
+                return lista_tipo_incidencias = repo_tipo_incidencia.GetAll();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
