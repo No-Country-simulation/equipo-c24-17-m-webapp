eced4c56c5e3237130ec022729768f6f7dbfc520
Elliot <unforriven@gmail.com>
2025-03-04 15:18:20 -0300

    Responsive Footer v1

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Showcase from "./showcase";
 import Conocenos from "./conocenos";
+import { Footer } from "@/components/Footer";
 
 export default async function Home() {
 	return (
@@ -8,6 +9,7 @@ export default async function Home() {
 			<Header />
 			<Showcase />
 			<Conocenos />
+			<Footer />
 		</div>
 	);
 }
--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -7,14 +7,14 @@ import Link from "next/link";
 
 
 export function Footer() {
-    
+
     return (
         <footer className="bg-[#BED2FA] relative">
-            <div className="container h-[max-content] w-[100dvw] py-12 pl-12 flex gap-[4rem]">
+            <div className="container h-[max-content] w-[100dvw] py-12 flex flex-col justify-center items-center md:flex-row md:pl-12 md:justify-around">
                 <section className="pl-4 flex gap-8">
                     <div>
                         <div className="pr-4 py-4">
-                            <Link href={"/#"}>
+                            <Link href={"/"}>
                                 <Image src={"/logo.svg"} alt="Logo TEA" width={240} height={60} />
                             </Link>
                         </div>
@@ -22,47 +22,48 @@ export function Footer() {
                         <div className="w-[250px] h-[1px] bg-black" />
 
                         <div className="flex flex-col gap-8 pb-12">
-                            <h3 className="font-bold pt-4 text-[#5881C1] text-2xl">Contacto</h3>
-                            <span className="flex gap-2"><MapPin />Buenos Aires, Argentina</span>
-                            <span className="flex gap-2"><Mail />teacompsoporte@gmail.com</span>
+                            <h3 className="font-bold pt-4 text-[#5881C1] text-xl">Contacto</h3>
+                            <span className="flex gap-2 text-sm"><MapPin />Buenos Aires, Argentina</span>
+                            <span className="flex gap-2 text-sm"><Mail />teacompsoporte@gmail.com</span>
                         </div>
                     </div>
 
-                    <div className="w-[1px] h-[300px] bg-black" />
+                    <div className="w-[1px] h-[300px] bg-black hidden md:block" />
 
                 </section>
 
-                <section className="flex flex-col gap-8 p-8 mx-12">
+                <div className="flex">
+                    <section className="flex flex-col gap-8 p-8 min-[850px]:mx-12">
 
-                    <h2 className="text-[#FA4A0C] text-2xl">Recursos</h2>
+                        <h2 className="text-[#FA4A0C] text-2xl">Recursos</h2>
 
-                    <div className="flex flex-col gap-2">
-                        <h3>Comunidad</h3>
-                        <h3>Guías de apoyo</h3>
-                        <h3>Crea tu perfil</h3>
-                        <h3>FAQ's</h3>
-                    </div>
+                        <div className="flex flex-col gap-2">
+                            <small className="hover:underline cursor-pointer">Comunidad</small>
+                            <small className="hover:underline cursor-pointer">Guías de apoyo</small>
+                            <small className="hover:underline cursor-pointer">Crea tu perfil</small>
+                            <small className="hover:underline cursor-pointer">FAQ's</small>
+                        </div>
 
-                </section>
+                    </section>
 
-                <section className="flex flex-col gap-8 p-8 mx-12">
+                    <section className="flex flex-col gap-8 p-8 min-[850px]:mx-12">
 
-                    <h2 className="text-[#FA4A0C] text-2xl">Recursos</h2>
-                    <div className="flex flex-col gap-2">
-                        <h3>Comunidad</h3>
-                        <h3>Guías de apoyo</h3>
-                        <h3>Crea tu perfil</h3>
-                        <h3>FAQ's</h3>
-                    </div>
+                        <h2 className="text-[#FA4A0C] text-2xl">Nosotros</h2>
+                        <div className="flex flex-col gap-2">
+                            <small className="hover:underline cursor-pointer">Quiénes Somos</small>
+                            <small className="hover:underline cursor-pointer">Información útil</small>
+                            <small className="hover:underline cursor-pointer">Contacto</small>
+                        </div>
 
-                </section>
+                    </section>
+                </div>
             </div>
             <button
                 onClick={(e) => {
                     e.preventDefault();
                     window.scrollTo({ top: 0, behavior: "smooth" });
                 }}
-                className="absolute right-10 bottom-10 text-[#FA4A0C] bg-[#00000085] p-1.5 rounded-full">
+                className="absolute right-10 bottom-10 text-[#FA4A0C] bg-[#00000085] p-1.5 rounded-full hover:shadow-[0_0_12px_#000] transition-all">
                 <ArrowUp />
             </button>
         </footer>
