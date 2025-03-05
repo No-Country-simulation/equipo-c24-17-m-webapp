a201a465e3d3c53d5a56870da76069245c1c56a8
Matias Luduena <matiasluduena23@gmail.com>
2025-03-05 07:58:38 -0300

    ft-front-sidebar

---
+++ client/components/sidebar-menu.tsx
@@ -0,0 +1,55 @@
+import {
+	SidebarGroup,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuSub,
+	SidebarMenuSubButton,
+	SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
+import { ChevronRight } from "lucide-react";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "./ui/collapsible";
+import { getParientes, getSession } from "@/lib/database";
+import Link from "next/link";
+
+export default async function SidebarMain() {
+	const { email } = await getSession();
+	const familiares = await getParientes(email);
+	return (
+		<SidebarGroup>
+			<SidebarGroupLabel>Accesos</SidebarGroupLabel>
+			<SidebarMenu>
+				<Collapsible className="group/collapsible">
+					<CollapsibleTrigger asChild>
+						<SidebarMenuButton className="border-full bg-blueCl text-white hover:bg-blueCl hover:text-white hover:opacity-80">
+							Familiares
+							<ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+						</SidebarMenuButton>
+					</CollapsibleTrigger>
+					<CollapsibleContent>
+						<SidebarMenuSub>
+							{familiares.map(({ id, nombre, apellido }) => (
+								<SidebarMenuSubItem key={id}>
+									<SidebarMenuSubButton asChild>
+										<Link href={`/panel/familiares/${id}`}>
+											{apellido}, {nombre}
+										</Link>
+									</SidebarMenuSubButton>
+								</SidebarMenuSubItem>
+							))}
+							<SidebarMenuSubItem>
+								<SidebarMenuSubButton asChild>
+									<Link href={`/panel/familiares/crear`}>Agregar Familiar</Link>
+								</SidebarMenuSubButton>
+							</SidebarMenuSubItem>
+						</SidebarMenuSub>
+					</CollapsibleContent>
+				</Collapsible>
+			</SidebarMenu>
+		</SidebarGroup>
+	);
+}
