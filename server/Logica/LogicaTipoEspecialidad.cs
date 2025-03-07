1115e32c4c7c9431fd550fb45a9df72a2799e6e7
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-03-07 19:36:20 -0300

    logica de obtener todos las especialidades

---
+++ server/Logica/LogicaTipoEspecialidad.cs
@@ -0,0 +1,33 @@
+﻿using server.Data.Models;
+using server.Data.Repositorios;
+using server.Data;
+
+namespace server.Logica
+{
+    public class LogicaTipoEspecialidad
+    {
+        #region ContextDataBase
+        private readonly BdTeacompanioContext _context;
+
+        public LogicaTipoEspecialidad(BdTeacompanioContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        public List<TipoEspecialidad> ObtenerTodosLosEstudiosMedicos(int id)
+        {
+            RepoTipoEspecialidad repo_especialidad = new RepoTipoEspecialidad(_context);
+            List<TipoEspecialidad> lista_especialistas = [];
+
+            try
+            {
+                return lista_especialistas = repo_especialidad.GetAll();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
